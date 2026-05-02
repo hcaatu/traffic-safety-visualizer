71f2450048ad3ab01ff86082c405a6cf7cfd9b99
Aatu Hokkanen <aatu.hokkane@gmail.com>
2026-05-02 19:07:48 +0300

    added accident type filter

--- app.py
+++ app.py
@@ -10,20 +10,33 @@ df = pd.read_csv(
     sep=";"
 )
 
-gdf = gp.GeoDataFrame(
-    df,
-    geometry=gp.points_from_xy(
-        df.ita_etrs, df.pohj_etrs
-    ),
-    crs="EPSG:3879"
-)
+def filter_by_accident_type(input_df: pd.DataFrame, filter: str) -> pd.DataFrame:
+    if filter == "none":
+        return input_df
+    filtered_df = input_df.copy()
+    print(filtered_df.shape)
+    filtered_df = filtered_df[filtered_df["Type of accident"] == filter]
+    print(filtered_df.shape)
+    return filtered_df
 
-gdf = gdf.to_crs(epsg=4326)
+def get_gdf(df):
+    gdf = gp.GeoDataFrame(
+        df,
+        geometry=gp.points_from_xy(
+            df.ita_etrs, df.pohj_etrs
+        ),
+        crs="EPSG:3879"
+    )
 
-df["lon"] = gdf.geometry.x
-df["lat"] = gdf.geometry.y
+    gdf = gdf.to_crs(epsg=4326)
+    return gdf
 
-geojson = gdf.__geo_interface__
+def get_geojson(gdf):
+    df["lon"] = gdf.geometry.x
+    df["lat"] = gdf.geometry.y
+
+    geojson = gdf.__geo_interface__
+    return geojson
 
 # For the area plot
 df_counts = (
@@ -33,56 +46,83 @@ df_counts = (
     .reset_index(name="count")
 )
 
-df_counts = df_counts.rename({"VV": "Year", "LAJI": "Type of accident"}, axis=1)
-category_map = {
-    "JK": "pedestrian",
-    "MA": "motor vehicle",
-    "MP": "motorcycle",
-    "PP": "bicycle"
-}
+def rename_columns(input_df):
+    df = input_df.copy()
+    df = df.rename({
+        "VV": "Year", 
+        "LAJI": "Type of accident",
+        "VAKAV_A": "Severity"
+    }, axis=1)
 
-df_counts["Type of accident"] = df_counts["Type of accident"].map(category_map)
+    category_map = {
+        "JK": "pedestrian",
+        "MA": "motor vehicle",
+        "MP": "motorcycle",
+        "PP": "bicycle"
+    }
+
+    df["Type of accident"] = df["Type of accident"].map(category_map)
+
+    return df
+
+df = rename_columns(df)
+df_counts = rename_columns(df_counts)
 
 
 # --- PLOTLY ---
 
-fig = px.choropleth_map(
-    gdf,
-    geojson=geojson,
-    locations=gdf.index,
-    map_style="basic",
-    zoom=10,
-    center={"lat": df["lat"].mean(), "lon": df["lon"].mean()},
-    opacity=0.8
-)
+def plot_heatmap(gdf, geojson):
+    print(f"plotted gdf shape {gdf.shape}")
+    fig = px.choropleth_map(
+        gdf,
+        geojson=geojson,
+        locations=gdf.index,
+        map_style="basic",
+        zoom=10,
+        center={"lat": df["lat"].mean(), "lon": df["lon"].mean()},
+        opacity=0.8
+    )
 
-fig.add_trace(
-    px.density_map(
+    fig.add_trace(
+        px.density_map(
+            df,
+            lat="lat",
+            lon="lon",
+            z="Severity",  # intensity
+            radius=15,
+            opacity=0.5,
+            color_continuous_scale="Blues",
+        ).data[0]
+    )
+
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    return fig
+
+def plot_area(df):
+    fig2 = px.area(
         df,
-        lat="lat",
-        lon="lon",
-        z="VAKAV_A",  # intensity
-        radius=15,
-        opacity=0.5,
-        color_continuous_scale="Blues",
-    ).data[0]
-)
-
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-fig2 = px.area(
-    df_counts,
-    x="Year",
-    y="count",
-    color="Type of accident",
-)
+        x="Year",
+        y="count",
+        color="Type of accident",
+    )
+    return fig2
 
 # --- STREAMLIT ---
 
 st.title("Helsinki Traffic Safety Analysis")
 
-st.plotly_chart(fig, width='stretch', theme=None)
+option = st.selectbox(
+    "Filter by accident type",
+    ("none", "motor vehicle", "motorcycle", "bicycle", "pedestrian")
+)
 
-st.plotly_chart(fig2, width='stretch', theme=None)
+df = filter_by_accident_type(df, option)
+gdf = get_gdf(df)
+geojson = get_geojson(gdf)
+
+st.plotly_chart(plot_heatmap(gdf, geojson), width='stretch', theme=None)
+
+st.plotly_chart(plot_area(df_counts), width='stretch', theme=None)
 
 
